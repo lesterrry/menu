8b31f8848c57c2ae7e799177b712f3a8bcbb7258
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-04-03 16:46:48 +0300

    workin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'menu/index'
+  root 'menu#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
